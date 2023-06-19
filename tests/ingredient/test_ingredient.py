3d861e4d62bbb6148d53206d4532fbbcd0dfff95
Igor Arecippo <iarecippo@gmail.com>
2023-06-19 18:23:32 -0300

    feat: Testing class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,33 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # Garanta que o nome passado no construtor está correto no atributo name
+    ingredient = Ingredient("queijo mussarela")
+    assert ingredient.name == "queijo mussarela"
+
+    # Garanta que o atributo restrictions seja preenchido corretamente
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+
+    # Garanta que o método __repr__ tenha o comportamento esperado
+    ingredient = Ingredient("bacon")
+    assert repr(ingredient) == "Ingredient('bacon')"
+
+    # Garanta que o operador de igualdade identifique ingredientes iguais
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+
+    # Garanta que hashs de ingredientes iguais também sejam iguais
+    assert hash(ingredient1) == hash(ingredient2)
+
+    # Garanta que hashs de ingredientes diferentes também sejam diferentes
+    assert hash(ingredient1) != hash(ingredient3)
