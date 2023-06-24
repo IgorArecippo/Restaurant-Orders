85a2beb2760747a9efff5a8f8266fc01fd5df41c
Igor Arecippo <iarecippo@gmail.com>
2023-06-24 15:07:34 -0300

    feat: func get_main_menu

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,15 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        return [
+            {
+                'dish_name': dish.name,
+                'ingredients': dish.get_ingredients(),
+                'price': dish.price,
+                'restrictions': list(dish.get_restrictions())
+            }
+            for dish in self.menu_data.dishes
+            if not (restriction and restriction in dish.get_restrictions())
+            and all(ingredient in self.inventory.inventory
+                    for ingredient in dish.get_ingredients())
+        ]
